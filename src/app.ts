961a69d58ea4dd3c442abd1ee0060282b578ed31
João Daniel <jotaf.daniel@gmail.com>
2020-11-29 10:47:37 -0300

    add JWT handling
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,20 @@ import * as bodyparser from 'koa-bodyparser';
 import * as logger from 'koa-logger';
 import * as json from 'koa-json';
 import * as cors from '@koa/cors';
+import * as jwt from 'jsonwebtoken';
 
-const corsConfig: any =
-  process.env.NODE_ENV == 'production'
-    ? { origin: 'https://themicroservicesinfo.netlify.app' }
-    : { origin: '*' };
+const secret: string = process.env.APP_SECRET as string;
+
+if (!secret) {
+  console.error('[!] APP_SECRET undefined! It must be defined!');
+  process.exit(1);
+}
 
 export const app: Koa = new Koa();
 app.use(bodyparser());
 app.use(logger());
 app.use(json());
-app.use(cors(corsConfig));
+app.use(cors());
 
 const router: Router = new Router();
 
@@ -89,6 +92,22 @@ const validate: (data: any) => ValidationOutput = (data: any) => {
   };
 };
 
+app.use(
+  async (ctx: Koa.Context, next: any): Promise<void> => {
+    try {
+      const { token } = ctx.request.body;
+      const payload = jwt.verify(token, secret);
+      ctx.request.body = payload;
+      await next();
+    } catch (e) {
+      ctx.request.body = null;
+      console.dir(e);
+      ctx.body = { error: 'Invalid payload' };
+      ctx.status = 401;
+    }
+  }
+);
+
 router.post(
   '/answers',
   async (ctx: Koa.Context): Promise<void> => {
@@ -97,11 +116,12 @@ router.post(
     if (!isValid) {
       ctx.body = { errors: reasons };
       ctx.status = 400;
-    } else {
-      await ctx.db.collection('answers').insertOne(ctx.request.body);
-      ctx.body = { msg: 'ok' };
-      ctx.status = 201;
+      return;
     }
+    const { ops } = await ctx.db.collection('answers').insertOne(ctx.request.body);
+
+    ctx.status = 201;
+    ctx.body = { answers: ops };
   }
 );
 
--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,16 @@ import { MongoClient } from 'mongodb';
 import { app } from './app';
 
 const mongoURL = process.env.MONGO_URL || 'mongo://localhost:27017';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 const mongoClient = new MongoClient(mongoURL);
 
 async function bootstrap() {
-  try {
-    await mongoClient.connect();
+  await mongoClient.connect();
 
-    app.context.db = mongoClient.db('themsinfo');
+  app.context.db = mongoClient.db('themsinfo');
 
-    app.listen(port, () => console.log('=== Server Running!\n\n\n'));
-  } finally {
-    await mongoClient.close();
-  }
+  app.listen(port, () => console.log('=== Server Running!\n\n\n'));
 }
 
 bootstrap().catch(console.dir);
